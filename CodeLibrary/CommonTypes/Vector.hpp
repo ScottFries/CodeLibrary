4c72f189dc497e4e46468768cc92f3abc1dba268
Scott Fries <Scott@RealFakeWorld.com>
2018-07-15 19:36:28 -0700

    Added basic Vector type.

---
+++ CodeLibrary/CommonTypes/Vector.hpp
@@ -0,0 +1,358 @@
+#pragma once
+
+#include <array>
+
+namespace balance
+{
+template <typename T, size_t N>
+class Vector;
+
+/// Common type aliases
+using Vector2f = Vector<float, 2>;
+using Vector3f = Vector<float, 3>;
+using Vector4f = Vector<float, 4>;
+using Vector2d = Vector<double, 2>;
+using Vector3d = Vector<double, 3>;
+using Vector4d = Vector<double, 4>;
+
+template <typename T, size_t N>
+class BaseVector
+{
+public:
+    constexpr BaseVector() = default;
+
+    constexpr BaseVector(std::array<T, N> vals)
+        : data_(vals)
+    {}
+
+    // TODO: Too greedy for common use; may offer as make_vector for higher order Vectors
+    //template <typename... Args>
+    //BaseVector(Args&&... args)
+    //    : data_({ std::forward<Args>(args)... })
+    //{}
+
+    constexpr BaseVector(const BaseVector<T, N>&) = default;
+    constexpr BaseVector(BaseVector<T, N>&&) = default;
+    constexpr BaseVector<T, N>& operator=(const BaseVector<T, N>&) = default;
+    constexpr BaseVector<T, N>& operator=(BaseVector<T, N>&&) = default;
+    ~BaseVector() = default;
+
+    constexpr BaseVector<T, N> operator+(const BaseVector<T, N>& v) const
+    {
+        BaseVector<T, N> result;
+        for (size_t i = 0; i < N; ++i)
+            result.data_[i] = this->data_[i] + v.data_[i];
+        return result;
+    }
+
+    constexpr BaseVector<T, N>& operator+=(const BaseVector<T, N>& v)
+    {
+        for (size_t i = 0; i < N; ++i)
+            this->data_[i] += v.data_[i];
+        return *this;
+    }
+
+    constexpr BaseVector<T, N> operator-(const BaseVector<T, N>& v) const
+    {
+        BaseVector<T, N> result;
+        for (size_t i = 0; i < N; ++i)
+            result.data_[i] = this->data_[i] - v.data_[i];
+        return result;
+    }
+
+    constexpr BaseVector<T, N>& operator-=(const BaseVector<T, N>& v)
+    {
+        for (size_t i = 0; i < N; ++i)
+            this->data_[i] -= v.data_[i];
+        return *this;
+    }
+
+    constexpr BaseVector<T, N> operator*(const T& t) const
+    {
+        BaseVector<T, N> result = *this;
+        for (T& val : result.data_)
+            val *= t;
+        return result;
+    }
+
+    constexpr BaseVector<T, N>& operator*=(const T& t)
+    {
+        for (T& val : data_)
+            val *= t;
+        return *this;
+    }
+
+    constexpr T Dot(const BaseVector<T, N>& v) const
+    {
+        T result = {};
+        for (size_t i = 0; i < N; ++i)
+            result += this->data_[i] * v.data_[i];
+        return result;
+    }
+
+    constexpr BaseVector<T, N> Project(const BaseVector<T, N>& direction, bool direction_normalized = false)
+    {
+        BaseVector<T, N> normalized_direction = direction_normalized ? direction : Normal(direction);
+        return normalized_direction * Dot(normalized_direction);
+    }
+
+    constexpr void Normalize()
+    {
+        *this *= 1 / Length();
+    }
+
+    constexpr void Normalize(T length)
+    {
+        *this *= 1 / length;
+    }
+
+    constexpr T Length() const
+    {
+        return sqrt(LengthSquared()); // TODO: Can you determine T to pick which sqrt? Do you need to?
+    }
+
+    constexpr T LengthSquared() const
+    {
+        T result = {};
+        for (size_t i = 0; i < N; ++i)
+            result += this->data_[i] * this->data_[i];
+        return result;
+    }
+
+    constexpr const std::array<T, N>& Data() const { return data_; }
+
+protected:
+    std::array<T, N> data_ = {};
+};
+
+template <typename T, size_t N>
+class Vector : public BaseVector<T, N>
+{
+public:
+    using BaseVector<T, N>::BaseVector;
+    using BaseVector<T, N>::operator+;
+    using BaseVector<T, N>::operator+=;
+    using BaseVector<T, N>::operator-;
+    using BaseVector<T, N>::operator-=;
+    using BaseVector<T, N>::operator*;
+    using BaseVector<T, N>::operator*=;
+    using BaseVector<T, N>::Dot;
+    using BaseVector<T, N>::Project;
+    using BaseVector<T, N>::Normalize;
+    using BaseVector<T, N>::Length;
+    using BaseVector<T, N>::LengthSquared;
+    using BaseVector<T, N>::Data;
+
+    Vector() = default;
+
+    Vector(BaseVector<T, N> v)
+        : BaseVector<T, N>(v)
+    {}
+};
+
+template <typename T>
+class Vector<T, 2> : public BaseVector<T, 2>
+{
+    static constexpr size_t N = 2;
+public:
+    using BaseVector<T, N>::BaseVector;
+    using BaseVector<T, N>::operator+;
+    using BaseVector<T, N>::operator+=;
+    using BaseVector<T, N>::operator-;
+    using BaseVector<T, N>::operator-=;
+    using BaseVector<T, N>::operator*;
+    using BaseVector<T, N>::operator*=;
+    using BaseVector<T, N>::Dot;
+    using BaseVector<T, N>::Project;
+    using BaseVector<T, N>::Normalize;
+    using BaseVector<T, N>::Length;
+    using BaseVector<T, N>::LengthSquared;
+    using BaseVector<T, N>::Data;
+
+    Vector() = default;
+
+    Vector(BaseVector<T, N> v)
+        : BaseVector<T, N>(v)
+    {}
+
+    Vector(const T& x, const T& y)
+        : BaseVector<T, N>({ x, y })
+    {}
+
+    Vector(T&& x, T&& y)
+        : BaseVector<T, N>({ x, y })
+    {}
+
+    constexpr T& X() { return this->data_[0]; }
+    constexpr T& Y() { return this->data_[1]; }
+    constexpr const T& X() const { return this->data_[0]; }
+    constexpr const T& Y() const { return this->data_[1]; }
+};
+
+template <typename T>
+class Vector<T, 3> : public BaseVector<T, 3>
+{
+    static constexpr size_t N = 3;
+public:
+    using BaseVector<T, N>::BaseVector;
+    using BaseVector<T, N>::operator+;
+    using BaseVector<T, N>::operator+=;
+    using BaseVector<T, N>::operator-;
+    using BaseVector<T, N>::operator-=;
+    using BaseVector<T, N>::operator*;
+    using BaseVector<T, N>::operator*=;
+    using BaseVector<T, N>::Dot;
+    using BaseVector<T, N>::Project;
+    using BaseVector<T, N>::Normalize;
+    using BaseVector<T, N>::Length;
+    using BaseVector<T, N>::LengthSquared;
+    using BaseVector<T, N>::Data;
+
+    Vector() = default;
+
+    Vector(BaseVector<T, N> v)
+        : BaseVector<T, N>(v)
+    {}
+
+    Vector(const T& x, const T& y, const T& z)
+        : BaseVector<T, N>({ x, y, z })
+    {}
+
+    Vector(T&& x, T&& y, T&& z)
+        : BaseVector<T, N>({ x, y, z })
+    {}
+
+    constexpr T& X() { return this->data_[0]; }
+    constexpr T& Y() { return this->data_[1]; }
+    constexpr T& Z() { return this->data_[2]; }
+    constexpr const T& X() const { return this->data_[0]; }
+    constexpr const T& Y() const { return this->data_[1]; }
+    constexpr const T& Z() const { return this->data_[2]; }
+
+    constexpr Vector<T, 3> Cross(const Vector<T, 3>& v) const
+    {
+        return Vector<T, 3>{ {(this->data_[1] * v.data_[2] - this->data_[2] * v.data_[1]),
+            (this->data_[2] * v.data_[0] - this->data_[0] * v.data_[2]),
+            (this->data_[0] * v.data_[1] - this->data_[1] * v.data_[0]) }};
+    }
+};
+
+template <typename T>
+class Vector<T, 4> : public BaseVector<T, 4>
+{
+    static constexpr size_t N = 4;
+public:
+    using BaseVector<T, N>::BaseVector;
+    using BaseVector<T, N>::operator+;
+    using BaseVector<T, N>::operator+=;
+    using BaseVector<T, N>::operator-;
+    using BaseVector<T, N>::operator-=;
+    using BaseVector<T, N>::operator*;
+    using BaseVector<T, N>::operator*=;
+    using BaseVector<T, N>::Dot;
+    using BaseVector<T, N>::Project;
+    using BaseVector<T, N>::Normalize;
+    using BaseVector<T, N>::Length;
+    using BaseVector<T, N>::LengthSquared;
+    using BaseVector<T, N>::Data;
+
+    Vector() = default;
+
+    Vector(BaseVector<T, N> v)
+        : BaseVector<T, N>(v)
+    {}
+
+    Vector(const T& x, const T& y, const T& z, const T& w)
+        : BaseVector<T, N>({ x, y, z, w })
+    {}
+
+    Vector(T&& x, T&& y, T&& z, T&& w)
+        : BaseVector<T, N>({ x, y, z, w })
+    {}
+
+    Vector(const BaseVector<T, 3>& v, const T& w)
+        : BaseVector<T, N>({ v.X(), v.Y(), v.Z(), w })
+    {}
+
+    Vector(BaseVector<T, 3>&& v, T&& w)
+        : BaseVector<T, N>({ v.X(), v.Y(), v.Z(), w })
+    {}
+
+    constexpr explicit operator Vector<T, 3>() const
+    {
+        return Vector<T, 3>(data_[0], this->data_[1], this->data_[2]);
+    }
+
+    constexpr T& X() { return this->data_[0]; }
+    constexpr T& Y() { return this->data_[1]; }
+    constexpr T& Z() { return this->data_[2]; }
+    constexpr T& W() { return this->data_[3]; }
+    constexpr const T& X() const { return this->data_[0]; }
+    constexpr const T& Y() const { return this->data_[1]; }
+    constexpr const T& Z() const { return this->data_[2]; }
+    constexpr const T& W() const { return this->data_[3]; }
+};
+
+template <typename T, size_t N>
+constexpr bool operator==(const Vector<T, N>& l, const Vector<T, N>& r)
+{
+    for (size_t i = 0; i < N; ++i)
+    {
+        if (l.Data()[i] != r.Data()[i])
+            return false;
+    }
+    return true;
+}
+
+template <typename T, size_t N>
+constexpr bool operator!=(const Vector<T, N>& l, const Vector<T, N>& r)
+{
+    return !(l == r);
+}
+
+template <typename T, size_t N>
+constexpr T Dot(const Vector<T, N>& l, const Vector<T, N>& r)
+{
+    return l.Dot(r);
+}
+
+template <typename T>
+constexpr Vector<T, 3> Cross(const Vector<T, 3>& l, const Vector<T, 3>& r)
+{
+    return l.Cross(r);
+}
+
+template <typename T, size_t N>
+constexpr Vector<T, N> Project(const Vector<T, N>& v, const Vector<T, N>& direction, bool direction_normalized = false)
+{
+    return v.Project(direction, direction_normalized);
+}
+
+template <typename T, size_t N>
+constexpr BaseVector<T, N> Normal(const BaseVector<T, N>& v)
+{
+    BaseVector<T, N> result(v);
+    result.Normalize();
+    return result;
+}
+
+template <typename T, size_t N>
+constexpr BaseVector<T, N> Normal(const BaseVector<T, N>& v, T length)
+{
+    BaseVector<T, N> result(v);
+    result.Normalize(length);
+    return result;
+}
+
+template <typename T, size_t N>
+constexpr T Length(const Vector<T, N>& v)
+{
+    return v.Length();
+}
+
+template <typename T, size_t N>
+constexpr T LengthSquared(const Vector<T, N>& v)
+{
+    return v.LengthSquared();
+}
+}
---
+++ CodeLibraryTest/CommonTypes/VectorTest.cpp
@@ -0,0 +1,256 @@
+#include "CommonTypes/Vector.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace balance;
+
+TEST(VectorConstructionTest, DefaultConstruction)
+{
+    Vector3f v;
+    ASSERT_FLOAT_EQ(v.X(), 0.0f);
+    ASSERT_FLOAT_EQ(v.Y(), 0.0f);
+    ASSERT_FLOAT_EQ(v.Z(), 0.0f);
+}
+
+TEST(VectorConstructionTest, AggregateConstruction)
+{
+    float x = 4.2f;
+    float y = 1.9f;
+    float z = -2.1f;
+
+    auto AssertVectorEqualsXYZ = [&x, &y, &z](Vector3f& vec)
+    {
+        ASSERT_FLOAT_EQ(vec.X(), x);
+        ASSERT_FLOAT_EQ(vec.Y(), y);
+        ASSERT_FLOAT_EQ(vec.Z(), z);
+    };
+
+    Vector3f v{ { x, y, z } };
+    // TODO: Syntax not supported with inheritance approach?
+    //Vector3f v2({ x, y, z });
+    AssertVectorEqualsXYZ(v);
+    //AssertVectorEqualsXYZ(v2);
+
+    std::array<float, 3> vals = { x, y, z };
+    Vector3f v3 = vals;
+    Vector3f v4 = { vals };
+    AssertVectorEqualsXYZ(v3);
+    AssertVectorEqualsXYZ(v4);
+}
+
+TEST(VectorConstructionTest, CopyConstruction)
+{
+    float x = 6.2f;
+    float y = 10.2f;
+    float z = 1.21f;
+
+    auto AssertVectorEqualsXYZ = [&x, &y, &z](Vector3f& vec)
+    {
+        ASSERT_FLOAT_EQ(vec.X(), x);
+        ASSERT_FLOAT_EQ(vec.Y(), y);
+        ASSERT_FLOAT_EQ(vec.Z(), z);
+    };
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = v;
+    Vector3f v3(v);
+    AssertVectorEqualsXYZ(v);
+    AssertVectorEqualsXYZ(v2);
+    AssertVectorEqualsXYZ(v3);
+}
+
+TEST(VectorConstructionTest, MoveConstruction)
+{
+    float x = 23.5f;
+    float y = -124.53f;
+    float z = 12.35f;
+
+    auto AssertVectorEqualsXYZ = [&x, &y, &z](Vector3f& vec)
+    {
+        ASSERT_FLOAT_EQ(vec.X(), x);
+        ASSERT_FLOAT_EQ(vec.Y(), y);
+        ASSERT_FLOAT_EQ(vec.Z(), z);
+    };
+
+    Vector3f v = { { x, y, z } };
+    AssertVectorEqualsXYZ(v);
+
+    Vector3f v2 = std::move(v);
+    AssertVectorEqualsXYZ(v2);
+
+    Vector3f v3(Vector3f{ { x, y, z } });
+    AssertVectorEqualsXYZ(v3);
+}
+
+
+TEST(VectorOperatorTest, Equality)
+{
+    float x = 12.532f;
+    float y = 932.25f;
+    float z = 12521.2f;
+    float x2 = -125.325f;
+    float y2 = 312.125f;
+    float z2 = 326.12f;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    Vector3f v3 = { { x, y, z } };
+    ASSERT_EQ(v, v);
+    ASSERT_NE(v, v2);
+    ASSERT_EQ(v, v3);
+}
+
+TEST(VectorOperatorTest, Addition)
+{
+    float x = 12.532f;
+    float y = 932.25f;
+    float z = 12521.2f;
+    float x2 = -125.325f;
+    float y2 = 312.125f;
+    float z2 = 326.12f;
+    float x3 = x + x2;
+    float y3 = y + y2;
+    float z3 = z + z2;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    Vector3f v3 = { { x3, y3, z3 } };
+
+    Vector3f v4 = v + v2;
+    ASSERT_EQ(v3, v4);
+    v += v2;
+    ASSERT_EQ(v, v3);
+}
+
+TEST(VectorOperatorTest, Subtraction)
+{
+    float x = 12.532f;
+    float y = 932.25f;
+    float z = 12521.2f;
+    float x2 = -125.325f;
+    float y2 = 312.125f;
+    float z2 = 326.12f;
+    float x3 = x - x2;
+    float y3 = y - y2;
+    float z3 = z - z2;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    Vector3f v3 = { { x3, y3, z3 } };
+
+    Vector3f v4 = v - v2;
+    ASSERT_EQ(v3, v4);
+    v -= v2;
+    ASSERT_EQ(v, v3);
+}
+
+TEST(VectorOperatorTest, Multiplication)
+{
+    float x = 12.532f;
+    float y = 463.214f;
+    float z = 1215.632f;
+    float t = -12.62f;
+    float x2 = x * t;
+    float y2 = y * t;
+    float z2 = z * t;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    Vector3f v3 = v * t;
+    ASSERT_EQ(v2, v3);
+    v *= t;
+    ASSERT_EQ(v, v2);
+}
+
+TEST(VectorFunctionTest, Dot)
+{
+    float x = 12.532f;
+    float y = 932.25f;
+    float z = 12521.2f;
+    float x2 = -125.325f;
+    float y2 = 312.125f;
+    float z2 = 326.12f;
+    float x3 = x * x2;
+    float y3 = y * y2;
+    float z3 = z * z2;
+    float sum = x3 + y3 + z3;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    float dot = v.Dot(v2);
+    float dot2 = v2.Dot(v);
+    float dot3 = Dot(v, v2);
+    EXPECT_FLOAT_EQ(dot, sum);
+    EXPECT_FLOAT_EQ(dot2, sum);
+    EXPECT_FLOAT_EQ(dot3, sum);
+}
+
+TEST(VectorFunctionTest, Cross)
+{
+    float x = 12.532f;
+    float y = 932.25f;
+    float z = 12521.2f;
+    float x2 = -125.325f;
+    float y2 = 312.125f;
+    float z2 = 326.12f;
+
+    Vector3f v = { { x, y, z } };
+    Vector3f v2 = { { x2, y2, z2 } };
+    Vector3f cross = Cross(v, v2);
+    ASSERT_FLOAT_EQ(cross.X(), (y * z2 - z * y2));
+    ASSERT_FLOAT_EQ(cross.Y(), (z * x2 - x * z2));
+    ASSERT_FLOAT_EQ(cross.Z(), (x * y2 - y * x2));
+}
+
+TEST(VectorFunctionTest, Project)
+{
+    float x = 125.532f;
+    float y = 346.23f;
+    float z = -12.532f;
+    float x2 = -52.12f;
+    float y2 = 346.23f;
+    float z2 = 63.3f;
+
+    Vector3f v = { {x, y, z} };
+    Vector3f v2 = { {x2, y2, z2} };
+    Vector3f v3 = v.Project(v2);
+    Vector3f v4 = v2 * (v.Dot(v2) / v2.LengthSquared());
+    ASSERT_FLOAT_EQ(v3.X(), v4.X());
+    ASSERT_FLOAT_EQ(v3.Y(), v4.Y());
+    ASSERT_FLOAT_EQ(v3.Z(), v4.Z());
+}
+
+TEST(VectorFunctionTest, Normalize)
+{
+    float x = 325.634f;
+    float y = 12.523f;
+    float z = 236.23f;
+
+    Vector3f v = { {x, y, z} };
+    Vector3f v2(v);
+    ASSERT_NE(v.Length(), 1.0f);
+    v.Normalize();
+    v2.Normalize(v2.Length());
+    ASSERT_FLOAT_EQ(v.Length(), 1.0f);
+    ASSERT_FLOAT_EQ(v2.Length(), 1.0f);
+}
+
+TEST(VectorFunctionTest, Length)
+{
+    float x = 325.634f;
+    float y = 12.523f;
+    float z = 236.23f;
+    float length_squared = x * x + y * y + z * z;
+
+    Vector3f v = { { x, y, z } };
+    ASSERT_FLOAT_EQ(v.LengthSquared(), length_squared);
+    ASSERT_FLOAT_EQ(v.Length(), sqrtf(length_squared));
+}
+
+int main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    int result = RUN_ALL_TESTS();
+    system("PAUSE");
+    return result;
+}
